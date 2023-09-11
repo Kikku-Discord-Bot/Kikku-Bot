264df8ac7e04fb337468f2a3e66692927c5aaf54
Tom <tomaugier23@gmail.com>
2023-09-11 10:05:50 +0200

    🚨 linter

--- src/commands/Event/CreateEvent.command.ts
+++ src/commands/Event/CreateEvent.command.ts
@@ -23,54 +23,54 @@ export class EventCreateCommand extends BaseCommand {
 	async execute(client: BaseClient, message: Message, args: string[]): Promise<void> {
 		const { ENV } = client.getKeys();
 
-        const embed = new EmbedBuilder()
-               .setTitle("Event")
-               .setDescription("Create an event")
-               .setColor(Colors.DarkButNotBlack)
-               .setTimestamp(new Date())
-        const row = new ActionRowBuilder<StringSelectMenuBuilder>()
-                .addComponents(
-                        new StringSelectMenuBuilder()
-                                .setCustomId("eventset")
-                                .setPlaceholder("Select an option")
-                                .addOptions([
-                                        {
-                                                label: "Create",
-                                                value: "create",
-                                                description: "Create an event",
-                                                emoji: "📅"
-                                        },
-                                        {
-                                                label: "Edit",
-                                                value: "edit",
-                                                description: "Edit an event",
-                                                emoji: "📝"
-                                        },
-                                        {
-                                                label: "Delete",
-                                                value: "delete",
-                                                description: "Delete an event",
-                                                emoji: "🗑️"
-                                        },
-                                    ])
-                );
+		const embed = new EmbedBuilder()
+			.setTitle("Event")
+			.setDescription("Create an event")
+			.setColor(Colors.DarkButNotBlack)
+			.setTimestamp(new Date())
+		const row = new ActionRowBuilder<StringSelectMenuBuilder>()
+			.addComponents(
+				new StringSelectMenuBuilder()
+					.setCustomId("eventset")
+					.setPlaceholder("Select an option")
+					.addOptions([
+						{
+							label: "Create",
+							value: "create",
+							description: "Create an event",
+							emoji: "📅"
+						},
+						{
+							label: "Edit",
+							value: "edit",
+							description: "Edit an event",
+							emoji: "📝"
+						},
+						{
+							label: "Delete",
+							value: "delete",
+							description: "Delete an event",
+							emoji: "🗑️"
+						},
+					])
+			);
 
-        if (ENV !== "dev") return;
+		if (ENV !== "dev") return;
 
-        console.log(args);
+		console.log(args);
 
-        message.channel.send({ embeds: [embed], components: [row] });
+		message.channel.send({ embeds: [embed], components: [row] });
 
 
 
-        // message.guild?.scheduledEvents.create({
-        //     name: "Test",
-        //     scheduledStartTime: 1684838886006,
-        //     description: "test",
-        //     channel: "1110513265764732988",
-        //     privacyLevel: GuildScheduledEventPrivacyLevel.GuildOnly,
-        //     entityType: GuildScheduledEventEntityType.Voice,
-        // })
+		// message.guild?.scheduledEvents.create({
+		//     name: "Test",
+		//     scheduledStartTime: 1684838886006,
+		//     description: "test",
+		//     channel: "1110513265764732988",
+		//     privacyLevel: GuildScheduledEventPrivacyLevel.GuildOnly,
+		//     entityType: GuildScheduledEventEntityType.Voice,
+		// })
 	}
 
 }
